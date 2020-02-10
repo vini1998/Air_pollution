f9d2596f7e479f6d98bd70bed392d8c6da71fd52
vini1998 <vinisasidharan@gmail.com>
2020-02-10 11:52:41 +0530

    shiny

--- air_pollution/Shiny/server.R
+++ air_pollution/Shiny/server.R
@@ -1,7 +1,37 @@
 library(shiny)
+options(shiny.maxRequestSize = 9*1024^2)
 
 shinyServer(
   function(input,output){
+    data <- reactive({
+      file1 <- input$file
+      if(is.null(file1)){return()}
+      read.table(file = file1$datapath, sep = input$sep, header = input$header, stringsAsFactors = input$stringAsFactors)
+      
+    })
+      
+    output$filedf <- renderTable({
+      if(is.null(data())){return()}
+      input$file
+    })
+   
+    output$sum <- renderTable({
+      if(is.null(data())){return()}
+      summary(data())
+      
+    }) 
+    
+    output$table <- renderTable({
+      if(is.null(data())){return()}
+      data()
+    })
+    output$tb <- renderUI({
+      if(is.null(data()))
+        h5("Powered by", tags$img(src = 'Rstudio-Ball.png', height=200, width=200))
+      else
+        tabsetPanel(tabPanel("About file", tableOutput("filedf")),tabPanel("Data", tableOutput("table")),tabPanel("Summary", tableOutput("sum")))
+      
+    })
     
     
   }
--- air_pollution/Shiny/ui.R
+++ air_pollution/Shiny/ui.R
@@ -4,8 +4,20 @@ shinyUI(fluidPage(
   
   titlePanel(title = "PM2.5 Predictor"),
   sidebarLayout(
-    sidebarPanel("Data Values"),
-    mainPanel("Accuracy of the prediction")
+    sidebarPanel(
+      fileInput("file","Upload the file"),
+      helpText("Default max. file size is 5MB"),
+      tags$hr(),
+      h5(helpText("Select the read.table parameters below")),
+      checkboxInput(inputId = 'header', label = 'Header', value = FALSE),
+      checkboxInput(inputId = 'stringAsFactors', label = 'stringAsFactors', FALSE),
+      br(),
+      radioButtons(inputId = 'sep', label = 'Separator', choices = c(Comma=',',Semicolon=';',Tab='\t',Space=''), selected = '\t')
+      
+    ),
+    mainPanel(
+      uiOutput("tb")
+    )
     
   )
 )
